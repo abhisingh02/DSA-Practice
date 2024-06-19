284f6f64a4dd90b86735ea44aa6dc8ee3642740a
Abhishek Singh <singh.abhishek.2017447@gmail.com>
2024-06-19 09:49:48 +0530

    file upload

---
+++ Array/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void printArray() {
+
+}
+
+int main() {
+
+    int n[15];
+
+    cout << "Value at 0 index " << n[0] << endl;
+
+    cout << endl << "Everything is Fine " << endl;
+
+    return 0;
+}
---
+++ Binary Search/bs.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key) {
+
+    int start =0;
+    int end = size-1;
+
+    //int mid = (start+end)/2;
+
+    int mid = start + (end-start)/2;
+
+    while (start <= end)
+    {
+        if(arr[mid]==key) {
+            return mid;
+        }
+
+        if(key>arr[mid]) {
+            start = mid+1;
+        }
+
+        else{
+            end = mid-1;
+        }
+
+        //mid = (start+end)/2;
+        mid = start + (end-start)/2;
+    }
+    return -1;
+    
+}
+
+int main() {
+
+    int even[6] = {2,4,6,8,12,18};
+    int odd[5] = {3,8,11,14,15};
+
+    int index = binarySearch(odd, 5, 8);
+
+    cout<<"Index of 8 is " << index << endl;
+
+    return 0;
+}
